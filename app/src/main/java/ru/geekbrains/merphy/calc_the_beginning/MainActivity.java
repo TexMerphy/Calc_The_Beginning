46f4ccf6c440180eea163b7e48573204970dac85
TexMerph <loshakovpv@yandex.ru>
2021-10-27 22:24:01 +0300

    Не очень получается описать взаимодействие с кнопками

--- app/src/main/java/ru/geekbrains/merphy/calc_the_beginning/MainActivity.java
+++ app/src/main/java/ru/geekbrains/merphy/calc_the_beginning/MainActivity.java
@@ -3,71 +3,78 @@ package ru.geekbrains.merphy.calc_the_beginning;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
-            //String instanceState;
-            //if (savedInstanceState == null){
-            //    instanceState = "Первый запуск!";
-            //}
-            //else{
-            //    instanceState = "Повторный запуск!";
-            //}
-           // Toast.makeText(getApplicationContext(), instanceState + " - onCreate()", Toast.LENGTH_SHORT).show();
+
         }
+
+
+
+
+    private void initButton1ClickListener() {
+        Button button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(button1ClickListener);
+    }
 
         @Override
         protected void onStart() {
             super.onStart();
-            //Toast.makeText(getApplicationContext(), "onStart()", Toast.LENGTH_SHORT).show();
+
         }
 
         @Override
         protected void onRestoreInstanceState(Bundle saveInstanceState){
             super.onRestoreInstanceState(saveInstanceState);
-            //Toast.makeText(getApplicationContext(), "Повторный запуск!! - onRestoreInstanceState()", Toast.LENGTH_SHORT).show();
+
         }
 
         @Override
         protected void onResume() {
             super.onResume();
-            //Toast.makeText(getApplicationContext(), "onResume()", Toast.LENGTH_SHORT).show();
+
         }
 
         @Override
         protected void onPause() {
             super.onPause();
-            //Toast.makeText(getApplicationContext(), "onPause()", Toast.LENGTH_SHORT).show();
+
         }
 
         @Override
         protected void onSaveInstanceState(Bundle saveInstanceState){
             super.onSaveInstanceState(saveInstanceState);
-            //Toast.makeText(getApplicationContext(), "onSaveInstanceState()", Toast.LENGTH_SHORT).show();
+
         }
 
         @Override
         protected void onStop() {
             super.onStop();
-            //Toast.makeText(getApplicationContext(), "onStop()", Toast.LENGTH_SHORT).show();
+
         }
 
         @Override
         protected void onRestart() {
             super.onRestart();
-            //Toast.makeText(getApplicationContext(), "onRestart()", Toast.LENGTH_SHORT).show();
+
         }
 
         @Override
         protected void onDestroy() {
             super.onDestroy();
-            //Toast.makeText(getApplicationContext(), "onDestroy()", Toast.LENGTH_SHORT).show();
+
         }
+
+    @Override
+    public void onClick(View v) {
+
     }
+}
 }
