58963867f4c04509fe7f4c6ee3d6555ae13e8777
TexMerph <loshakovpv@yandex.ru>
2021-10-28 19:19:20 +0300

    Не очень получилось описать кнопки

--- app/src/main/java/ru/geekbrains/merphy/calc_the_beginning/MainActivity.java
+++ app/src/main/java/ru/geekbrains/merphy/calc_the_beginning/MainActivity.java
@@ -21,7 +21,95 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void initButton1ClickListener() {
         Button button1 = findViewById(R.id.button1);
-        button1.setOnClickListener(button1ClickListener);
+        button1.setOnClickListener(v -> {
+
+        });
+    }
+    private void initButton2ClickListener() {
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(v -> {
+
+        });
+    }
+    private void initButton3ClickListener() {
+        Button button3 = findViewById(R.id.button3);
+        button3.setOnClickListener(v -> {
+
+        });
+    }
+    private void initButton4ClickListener() {
+        Button button4 = findViewById(R.id.button4);
+        button4.setOnClickListener(v -> {
+
+        });
+    }private void initButton5ClickListener() {
+        Button button5 = findViewById(R.id.button5);
+        button5.setOnClickListener(v -> {
+
+        });
+    }
+    private void initButton6ClickListener() {
+        Button button6 = findViewById(R.id.button6);
+        button6.setOnClickListener(v -> {
+
+        });
+    }
+    private void initButton7ClickListener() {
+        Button button7 = findViewById(R.id.button7);
+        button7.setOnClickListener(v -> {
+
+        });
+    }private void initButton8ClickListener() {
+        Button button8 = findViewById(R.id.button8);
+        button8.setOnClickListener(v -> {
+
+        });
+    }private void initButton9ClickListener() {
+        Button button9 = findViewById(R.id.button9);
+        button9.setOnClickListener(v -> {
+
+        });
+    }private void initButton0ClickListener() {
+        Button button0 = findViewById(R.id.button0);
+        button0.setOnClickListener(v -> {
+
+        });
+    }
+    private void initButton11ClickListener() {
+        Button button11 = findViewById(R.id.button11);
+        button11.setOnClickListener(v -> {
+
+        });
+    }
+    private void initButton12ClickListener() {
+        Button button12 = findViewById(R.id.button12);
+        button12.setOnClickListener(v -> {
+
+        });
+    }
+    private void initButton13ClickListener() {
+        Button button13 = findViewById(R.id.button13);
+        button13.setOnClickListener(v -> {
+
+        });
+    }
+    private void initButton14ClickListener() {
+        Button button14 = findViewById(R.id.button14);
+        button14.setOnClickListener(v -> {
+
+        });
+    }
+    private void initButton15ClickListener() {
+        Button button15 = findViewById(R.id.button15);
+        button15.setOnClickListener(v -> {
+
+        });
+    }
+    private void initButtonCClickListener() {
+        Button buttonC = findViewById(R.id.buttonC);
+        buttonC.setOnClickListener(v -> {
+
+        });
     }
 
         @Override
